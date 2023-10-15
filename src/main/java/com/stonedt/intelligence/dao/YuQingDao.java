d93562481dcaf3bb10cd3b778e4314f754b466b0
RonnieLEE <142022794+jiuhaLEE@users.noreply.github.com>
2023-10-15 21:13:39 +0800

    Add files via upload

---
+++ src/main/java/com/stonedt/intelligence/dao/YuQingDao.java
@@ -0,0 +1,22 @@
+package com.stonedt.intelligence.dao;
+
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class YuQingDao {
+    @Autowired
+    public MongoTemplate mongoTemplate;
+
+    public List<Document> find(){
+        Query query = new Query();
+        List<Document> docs = mongoTemplate.find(query, Document.class, "yuqing");
+        return docs;
+    }
+}
+
